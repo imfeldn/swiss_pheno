f82026373537837fc65ebd3aea3b8cb98c98479f
Koen Hufkens <koen.hufkens@gmail.com>
2023-08-15 15:02:42 +0200

    moving data and refactoring calibration routines

--- analysis/020_model_calibration_alternating.R
+++ analysis/020_model_calibration_alternating.R
@@ -13,21 +13,34 @@ library(phenor)
 phenovals <- c("maesh13d","mfags13d","mprua65d")
 
 ### create directory for new calibration with simulated annealing
-subDir <- paste0("sa_run_",Sys.Date(),"_class123_calibration_on_earlyperiod")
-dir.create(file.path("data/03_calibrations/",subDir), showWarnings = F)
+base_dir <- file.path(
+  "data/03_calibrations/",
+  paste0("sa_run_",Sys.Date(),"_class123")
+  )
+
+dir.create(
+  base_dir,
+  showWarnings = FALSE,
+  recursive = TRUE
+  )
 
 ### select seeds, model, years
 seeds <- c(1,7,22,31,45,57,63,99)
 mods <-  c("LIN", "TT", "TTs", "PTT", "PTTs","M1","M1s","AT")
-allyrs <- 1951:2020
 
+# list driver files
+driver_files <- list.files("data/02_pheno_net/","*.rds", full.names = TRUE)
+
+message("Calibrating models:")
 # --- run calibration for alternating years ----
 lapply(phenovals, function(pv) {
+
+  message(paste0("-- calibrating: ", pv))
 
   # read in the full driver files
   # to be subset below to select the
   # correct training and testing years
-
+  drivers <- readRDS(driver_files[grepl(pv, driver_files)])
 
   # create selection criteria
   # for training and testing
@@ -66,26 +79,55 @@ lapply(phenovals, function(pv) {
   stn_list_train <- pr_fm_subset(stn_list, selection$train)
   stn_list_test <- pr_fm_subset(stn_list, selection$test)
 
-  #   print("fit models")
-  #
-  #   mod_out <- pr_fit_comparison(random_seeds = seeds,
-  #                                models = mods,
-  #                                data = stn_list_train,
-  #                                method = "GenSA",
-  #                                control = list(max.call = 40000, temperature = 1000000000), #/
-  #                                par_ranges = system.file("extdata", "parameter_ranges.csv", package = "phenor",mustWork = TRUE),
-  #                                ncores = length(seeds))
-  #
-  #   ### set 9999 to NA for evaluation
-  #   for(ii in 1:length(mod_out$modelled)){
-  #     mod_out$modelled[[ii]]$predicted_values[mod_out$modelled[[ii]]$predicted_values > 1000] <- NA
-  #   }
-  #
-  #   save(mod_out, file = paste0("data/03_calibrations/",subDir,"/",pv,"/model_comparison_",pv,".RData"))
-  #   save(stn_list_train, file = paste0("data/03_calibrations/",subDir,"/",pv,"/stn_list_train_",pv,".RData"))
-  #   save(stn_list_test, file = paste0("data/03_calibrations/",subDir,"/",pv,"/stn_list_test_",pv,".RData"))
+    print("fit models")
 
+    mod_out <- pr_fit_comparison(
+      random_seeds = seeds,
+      models = mods,
+      data = stn_list_train,
+      method = "GenSA",
+      control = list(
+        max.call = 4,
+        temperature = 1000000000
+      ),
+      par_ranges = system.file(
+        "extdata",
+        "parameter_ranges.csv",
+        package = "phenor",
+        mustWork = TRUE
+        ),
+      ncores = length(seeds)
+    )
+
+    ### set 9999 to NA for evaluation
+    for(ii in 1:length(mod_out$modelled)){
+      mod_out$modelled[[ii]]$predicted_values[mod_out$modelled[[ii]]$predicted_values > 1000] <- NA
+    }
+
+    # create output directory
+    dir.create(
+      file.path(base_dir, pv),
+      recursive = TRUE,
+      showWarnings = FALSE
+    )
+
+    # save data as compressed RDS files
+    saveRDS(
+      mod_out,
+      file = file.path(base_dir,pv,paste0("model_comparison_",pv,".rds")),
+      compress = "xz"
+      )
+
+    saveRDS(
+      stn_list_train,
+      file = file.path(base_dir,pv,paste0("stn_list_train_",pv,".rds")),
+      compress = "xz"
+      )
+
+    saveRDS(
+      stn_list_test,
+      file = file.path(base_dir,pv,paste0("stn_list_test_",pv,".rds")),
+      compress = "xz"
+      )
 })
-
-
 
---
+++ data-raw/019_generate_model_drivers.R
@@ -0,0 +1,94 @@
+# Generate model drivers from climate
+# reconstruction data, will be subset
+# during model calibration into a
+# test and training datasets
+
+# load packages
+library(phenor)
+
+# select phenophases for calibration
+# phenovals <- c("mfags13d","mprua65d","maesh13d",
+#                "mtaro65d","manen65d","mmald65d",
+#                "mpyrc65d","mcora65d","mcora13d",
+#                "mtusf65d","mcarp65d","mlard13d")
+
+phenovals <- c("maesh13d","mfags13d","mprua65d")
+
+# where to save the data
+output_dir <- "data/02_pheno_net/"
+
+# create the directory if it doesn't exist
+dir.create(output_dir, showWarnings = FALSE, recursive = TRUE)
+
+message("Generating complete driver data (all years, no subsets)")
+# --- run calibration for alternating years ----
+for(pv in phenovals){
+
+  message(paste0("-- processing: ", pv))
+
+  # set filenames for driver data
+  meteo_file <- file.path("data-raw/02_pheno_net/",
+                          paste0("meteo_",pv,"_class123.RData")
+                          )
+
+  phenology_file <- file.path("data-raw/02_pheno_net/",
+                           paste0("meteo_",pv,"_class123.RData")
+  )
+
+  # load pheno data and meteorological data
+  load(meteo_file)
+  load(phenology_file)
+
+  # split out station names
+  stns_pv <- unique(pheno_meta$nat_abbr)
+
+  # create empty list to hold all data
+  stn_list <- list()
+
+  # convert data to phenor format
+  for(stn in 1:length(stns_pv)){
+
+    id <- stns_pv[stn]
+    ind <- which(pheno_meta$nat_abbr == id & pheno_meta$reference_year < 2021)
+    ltm <- apply(meteo_dat[rownames(meteo_dat) == id,,], 2, mean)
+
+    locs <- as.numeric(unlist(pheno_meta[ind[1],c("lat","lon")]))
+    stn_list[[stn]]  <- list(
+      site = id,
+      location = locs,
+      doy = c(-102:-1,1:263),
+      ltm = ltm, # long term mean temperature for a given location / ignored
+      transition_dates = pheno_meta$doy[ind],
+      year = pheno_meta$reference_year[ind],
+      Ti = t(meteo_dat[rownames(meteo_dat) == id,allyrs %in% pheno_meta$reference_year[ind],]),
+      Tmini = NULL, # tmin and max ignored / allowed to be empty
+      Tmaxi = NULL, #
+      Li = matrix(
+        rep(daylength(c(264:365,1:263),locs[1]),
+            length(ind)),
+        nrow = 365,
+        ncol = length(ind)
+        )
+      )
+  }
+
+  # add names to the list elements
+  names(stn_list) <- stns_pv
+
+  # flatten the phenocam nested list format
+  # for easier subsetting
+  stn_list <- pr_flatten(stn_list)
+
+  # get class (based on meteo data)
+  basename(meteo_file)
+
+  # construct filename
+  filename <- file.path(output_dir, paste0(pv, "_full_model_drivers.rds"))
+
+  # save data as a compressed RDS file
+  saveRDS(
+    stn_list,
+    filename,
+    compress = "xz"
+    )
+}
