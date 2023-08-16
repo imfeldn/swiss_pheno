9050277207a75baa4151e755d61afafd1c17bc18
Noemi Imfeld <noemi@climcal3.giub.unibe.ch>
2023-08-16 10:06:05 +0200

    updating pull request

--- analysis/020_model_calibration_alternating.R
+++ analysis/020_model_calibration_alternating.R
@@ -2,7 +2,7 @@
 # over the whole dataset
 
 ### load packages
-library(phenor)
+library(phenor);library(dplyr)
 
 ### select phenophases for calibration
 # phenovals <- c("mfags13d","mprua65d","maesh13d",
@@ -11,18 +11,19 @@ library(phenor)
 #                "mtusf65d","mcarp65d","mlard13d")
 
 phenovals <- c("maesh13d","mfags13d","mprua65d")
+classes <- "123_longseries"
 
 ### create directory for new calibration with simulated annealing
 base_dir <- file.path(
   "data/03_calibrations/",
-  paste0("sa_run_",Sys.Date(),"_class123")
-  )
+  paste0("sa_run_",Sys.Date(),"_class",classes)
+)
 
 dir.create(
   base_dir,
   showWarnings = FALSE,
   recursive = TRUE
-  )
+)
 
 ### select seeds, model, years
 seeds <- c(1,7,22,31,45,57,63,99)
@@ -40,13 +41,13 @@ lapply(phenovals, function(pv) {
   # read in the full driver files
   # to be subset below to select the
   # correct training and testing years
-  drivers <- readRDS(driver_files[grepl(pv, driver_files)])
+  drivers <- readRDS(driver_files[grepl(paste0(pv,".*",classes,".rds"), driver_files)])
 
   # create selection criteria
   # for training and testing
   selection <- data.frame(
-    site = stn_list$site,
-    year = stn_list$year
+    site = drivers$site,
+    year = drivers$year
   ) |>
     mutate(
       train = (year %% 2 == 0),
@@ -58,7 +59,7 @@ lapply(phenovals, function(pv) {
     dplyr::group_by(site) |>
     mutate(
       train = ifelse(
-        length(which(train)) > 1,
+        length(which(train)) > 1 & train == TRUE,
         TRUE,
         FALSE
       )
@@ -69,65 +70,64 @@ lapply(phenovals, function(pv) {
     dplyr::group_by(site) |>
     mutate(
       test = ifelse(
-        length(which(test)) > 1,
+        length(which(test)) > 1 & test == TRUE,
         TRUE,
         FALSE
       )
     )
 
   # subset training and testing datasets
-  stn_list_train <- pr_fm_subset(stn_list, selection$train)
-  stn_list_test <- pr_fm_subset(stn_list, selection$test)
+  stn_list_train <- pr_fm_subset(drivers, selection$train)
+  stn_list_test <- pr_fm_subset(drivers, selection$test)
 
-    print("fit models")
+  print("fit models")
 
-    mod_out <- pr_fit_comparison(
-      random_seeds = seeds,
-      models = mods,
-      data = stn_list_train,
-      method = "GenSA",
-      control = list(
-        max.call = 4,
-        temperature = 1000000000
-      ),
-      par_ranges = system.file(
-        "extdata",
-        "parameter_ranges.csv",
-        package = "phenor",
-        mustWork = TRUE
-        ),
-      ncores = length(seeds)
-    )
+  mod_out <- pr_fit_comparison(
+    random_seeds = seeds,
+    models = mods,
+    data = stn_list_train,
+    method = "GenSA",
+    control = list(
+      max.call = 4,
+      temperature = 1000000000
+    ),
+    par_ranges = system.file(
+      "extdata",
+      "parameter_ranges.csv",
+      package = "phenor",
+      mustWork = TRUE
+    ),
+    ncores = length(seeds)
+  )
 
-    ### set 9999 to NA for evaluation
-    for(ii in 1:length(mod_out$modelled)){
-      mod_out$modelled[[ii]]$predicted_values[mod_out$modelled[[ii]]$predicted_values > 1000] <- NA
-    }
+  ### set 9999 to NA for evaluation
+  for(ii in 1:length(mod_out$modelled)){
+    mod_out$modelled[[ii]]$predicted_values[mod_out$modelled[[ii]]$predicted_values > 1000] <- NA
+  }
 
-    # create output directory
-    dir.create(
-      file.path(base_dir, pv),
-      recursive = TRUE,
-      showWarnings = FALSE
-    )
+  # create output directory
+  dir.create(
+    file.path(base_dir, pv),
+    recursive = TRUE,
+    showWarnings = FALSE
+  )
 
-    # save data as compressed RDS files
-    saveRDS(
-      mod_out,
-      file = file.path(base_dir,pv,paste0("model_comparison_",pv,".rds")),
-      compress = "xz"
-      )
+  # save data as compressed RDS files
+  saveRDS(
+    mod_out,
+    file = file.path(base_dir,pv,paste0("model_comparison_",pv,".rds")),
+    compress = "xz"
+  )
 
-    saveRDS(
-      stn_list_train,
-      file = file.path(base_dir,pv,paste0("stn_list_train_",pv,".rds")),
-      compress = "xz"
-      )
+  saveRDS(
+    stn_list_train,
+    file = file.path(base_dir,pv,paste0("stn_list_train_",pv,".rds")),
+    compress = "xz"
+  )
 
-    saveRDS(
-      stn_list_test,
-      file = file.path(base_dir,pv,paste0("stn_list_test_",pv,".rds")),
-      compress = "xz"
-      )
+  saveRDS(
+    stn_list_test,
+    file = file.path(base_dir,pv,paste0("stn_list_test_",pv,".rds")),
+    compress = "xz"
+  )
 })
-
--- data-raw/010_stnsel_pheno.R
+++ data-raw/010_stnsel_pheno.R
@@ -5,8 +5,10 @@ library(ncdf4)
 library(stats)
 library(geosphere)
 
+### functions
+source("R/helpfuns.R")
 #### directories
-indir <- "wear/swiss_indices/"
+indir <- "../swiss_indices/"
 
 ### read station list
 station_list <- read.csv(paste0(indir,"/2_data/pheno_net/data/ch.meteoschweiz.messnetz-phaenologie_de.csv"),header = T, sep = ",", allowEscapes = T)
@@ -22,16 +24,12 @@ classification <- read.table(paste0(indir,"/2_data/pheno_net/dataD4.csv"),header
 qc <- read.csv(paste0(indir,"/2_data/pheno_net/PhenoClass/QCflags_minimum_Final_Juni2017.csv"),
                header=T, sep =",")
 
-### select stations with very valuable stations
-#mprua65d	Cherry tree - full flowering 662
-#maesh13d	Horse chestnut - leaf unfolding 601
-#mfags13d	European beech - leaf unfolding 606
-
-phenovals <- c("mfags13d", "mprua65d", "maesh13d","mcora65d","mcora13d","manen65d","mtaro65d","mtusf65d","mcarp65d","mlard13d")
-param_id <- c(606,662,601,666,664,619,617,654,656,653,658,634)
+### select stations based on classes
+phenovals <- c("mfags13d", "mprua65d", "maesh13d")#,"mcora65d","mcora13d","manen65d","mtaro65d","mtusf65d","mcarp65d","mlard13d")
+param_id <- c(606,662,601)#,666,664,619,617,654,656,653,658,634)
 names(param_id) <- phenovals
 
-classes <- "123"
+classes <- c(1,2,3)
 
 ###### create files based on classes ############
 for(pv in phenovals){
@@ -50,9 +48,9 @@ for(pv in phenovals){
   # save as compressed RDS
   saveRDS(
     station_list_sel,
-    file = paste0("data/02_pheno_net/stations_",pv,"_class",classes,".rds"),
+    file = paste0("data-raw/02_pheno_net/stations_",pv,"_class",cllps(classes),".rds"),
     compress = "xz"
-    )
+  )
 
   assign(paste0("station_list_",pv), station_list_sel)
   stnidx <- match(pheno_sel$nat_abbr, station_list_sel$Abk.)
@@ -62,25 +60,25 @@ for(pv in phenovals){
   N <- station_list_sel$KoordinatenN[stnidx]
   E <- station_list_sel$KoordinatenE[stnidx]
   alt <- station_list_sel$Stationshoehe[stnidx]
-  pheno_meta <- cbind(pheno_sel, lon, lat, E, N, alt)
+  pheno_dat <- cbind(pheno_sel, lon, lat, E, N, alt)
 
 
   ### add qc flags
   qc_pv <- qc[qc$OLD_SHORT_TX == pv & qc$FinalFlagCountUnique == 1,c("station","year","doy","value","FinalFlagCountUnique")]
 
   for(ii in 1:nrow(qc_pv)){
-    ind <- which(pheno_meta$nat_abbr == qc_pv[ii,"station"] & pheno_meta$reference_year == qc_pv[ii,"year"])
+    ind <- which(pheno_dat$nat_abbr == qc_pv[ii,"station"] & pheno_dat$reference_year == qc_pv[ii,"year"])
     if(length(ind) > 0){
-      print(pheno_meta[ind,])
-      pheno_meta[ind,"doy"] <- NA
+      print(pheno_dat[ind,])
+      pheno_dat[ind,"doy"] <- NA
     }
   }
 
   # save as compressed RDS
   saveRDS(
-    pheno_meta,
-    file = paste0("data/02_pheno_net/pheno_meta_",pv,"_class",classes,".rds"),
+    pheno_dat,
+    file = paste0("data-raw/02_pheno_net/pheno_dat_",pv,"_class",cllps(classes),".rds"),
     compress = "xz"
-    )
+  )
 
 }
